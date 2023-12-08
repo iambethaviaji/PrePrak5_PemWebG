0a68c19809fb312dca59e56622bc86376b1eb90c
iambethaviaji <127370627+iambethaviaji@users.noreply.github.com>
2023-12-08 17:47:34 +0700

    Add files via upload

---
+++ PemrogWebModul5/app/Config/DatabaseConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "preprak5";
+    public $port = 3306;
+}
